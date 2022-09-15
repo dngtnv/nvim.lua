89529264d30c64cd33495fd50c98298f01a167d9
dngtnv <kakalupin@gmail.com>
2022-09-15 16:00:13 +0700

    Update lspsaga config

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -32,3 +32,38 @@ map('n', '<A-9>', '<Cmd>BufferLineGoToBuffer 9<CR>', opts)
 map('n', '<A-0>', '<Cmd>BufferLineGoToBuffer -1<CR>', opts)
 -- Close buffer
 map('n', '<A-c>', '<Cmd>:Bdelete!<CR>', opts)
+
+-- Lspsaga --
+-- Lsp finder find the symbol definition implement reference
+-- if there is no implement it will hide
+-- when you use action in finder like open vsplit then you can
+-- use <C-t> to jump back
+map("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", opts)
+
+-- Code action
+--map("n", "v", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
+
+-- Rename
+map("n", "gr", "<cmd>Lspsaga rename<CR>", opts)
+
+-- Peek Definition
+-- you can edit the definition file in this flaotwindow
+-- also support open/vsplit/etc operation check definition_action_keys
+-- support tagstack C-t jump back
+map("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts)
+
+-- Show line diagnostics
+map("n", "<leader>cd", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
+
+-- Show cursor diagnostic
+map("n", "<leader>cd", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts)
+
+-- Diagnsotic jump can use `<c-o>` to jump back
+map("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
+map("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+
+-- Outline
+--keymap("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts)
+
+-- Hover Doc
+map("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
