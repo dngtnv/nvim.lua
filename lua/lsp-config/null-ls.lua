b2ee88217462e35d21ce4966a807a4bee44d60dd
dngtnv <kakalupin@gmail.com>
2022-09-18 16:37:27 +0700

    Add fixjson & prettierd formatter

--- lua/lsp-config/null-ls.lua
+++ lua/lsp-config/null-ls.lua
@@ -8,9 +8,10 @@ local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 
 local sources = {
     null_ls.builtins.diagnostics.eslint,
-    null_ls.builtins.formatting.prettier,
+    null_ls.builtins.formatting.prettierd,
     -- null_ls.builtins.diagnostics.luacheck,
     null_ls.builtins.formatting.stylua,
+    null_ls.builtins.formatting.fixjson,
     null_ls.builtins.code_actions.gitsigns,
 }
 
