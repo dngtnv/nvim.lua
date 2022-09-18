0caec015e51dafdc7dcb32a1bb9e6c5b14430783
dngtnv <kakalupin@gmail.com>
2022-09-18 19:58:57 +0700

    Update config null-ls & language-servers

--- lua/lsp-config/null-ls.lua
+++ lua/lsp-config/null-ls.lua
@@ -1,10 +1,10 @@
-local status_ok, null_ls = pcall(require, 'null-ls')
+local status_ok, null_ls = pcall(require, "null-ls")
 if not status_ok then
-    print('Null-ls is not installed')
+    print("Null-ls is not installed")
     return
 end
 
-local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local sources = {
     null_ls.builtins.diagnostics.eslint,
@@ -15,29 +15,27 @@ local sources = {
     null_ls.builtins.code_actions.gitsigns,
 }
 
-local callback = function()
-    vim.lsp.buf.format({
-        bufnr = bufnr,
-        filter = function(client)
-            return client.name == "null-ls"
+null_ls.setup({
+    sources = sources,
+    debug = false,
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                    --vim.lsp.buf.formatting_sync()
+                    vim.lsp.buf.format({
+                        timeout_ms = 3000,
+                        bufnr = bufnr,
+                        filter = function(client)
+                            return client.name == "null-ls"
+                        end,
+                    })
+                end,
+            })
         end
-    })
-end,
-
-    null_ls.setup({
-        sources = sources,
-        on_attach = function(client, bufnr)
-            if client.supports_method("textDocument/formatting") then
-                vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-                vim.api.nvim_create_autocmd("BufWritePre", {
-                    group = augroup,
-                    buffer = bufnr,
-                    callback = function()
-                        -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-                        --vim.lsp.buf.formatting_sync()
-                        vim.lsp.buf.format({ timeout_ms = 3000, bufnr = bufnr })
-                    end,
-                })
-            end
-        end,
-    })
+    end,
+})
