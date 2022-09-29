1eee449a406fef242bc74427e1db38f55ada4df3
dngtnv <kakalupin@gmail.com>
2022-09-29 20:07:31 +0700

    Update lualine config

--- lua/lualine-config/init.lua
+++ lua/lualine-config/init.lua
@@ -1,13 +1,41 @@
-local status_ok, lualine = pcall(require, 'lualine')
+local status_ok, lualine = pcall(require, "lualine")
 if not status_ok then
-    print('Lualine is not installed')
-    return
+	print("Lualine is not installed")
+	return
 end
 
-lualine.setup {
-    options = {
-        icons_enabled = true,
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
-    }
-}
+lualine.setup({
+	options = {
+		icons_enabled = true,
+		theme = "auto",
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
+		disabled_filetypes = {
+			statusline = { "NvimTree", "toggleterm" },
+		},
+		ignore_focus = { "NvimTree", "toggleterm" },
+		globalstatus = false,
+	},
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = { "branch", "diff", "diagnostics" },
+		lualine_c = {
+			{
+				"filename",
+				file_status = true,
+				newfile_status = false,
+				path = 1, -- [0,1,2,3]
+				shorting_target = 40,
+				sysbols = {
+					modified = "[+]",
+					readonly = "[-]",
+					unnamed = "[No Name]",
+					newfile = "[New]",
+				},
+			},
+		},
+		lualine_x = { "encoding", "fileformat", "filetype" },
+		lualine_y = { "progress" },
+		lualine_z = { "location" },
+	},
+})
