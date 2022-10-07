3323610af9fd7b4e01a2bf327a268ce3e572ceae
dngtnv <kakalupin@gmail.com>
2022-10-07 13:16:38 +0700

    refactor: update config nvim-tree & telescope

--- lua/telescope-config/init.lua
+++ lua/telescope-config/init.lua
@@ -1,24 +1,33 @@
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
-	print("Telescope is not installed")
-	return
+    print("Telescope is not installed")
+    return
 end
 
 telescope.setup({
-	defaults = {
-		winblend = 0,
-		border = {},
-		borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
-	},
-	pickers = {
-		find_files = {
-			-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
-			find_command = { "rg", "--files", "--no-ignore-vcs", "--hidden", "--glob", "!.git/*" },
-		},
-	},
-	file_ignore_patterns = {
-		"node_modules",
-		"build",
-		"dist",
-	},
+    defaults = {
+        vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+        },
+        winblend = 0,
+        border = {},
+        borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
+    },
+    pickers = {
+        find_files = {
+            -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+            find_command = { "rg", "--files", "--hidden", "--glob", "!.git/*" },
+        },
+    },
+    file_ignore_patterns = {
+        "node_modules",
+        "build",
+        "dist",
+    },
 })
