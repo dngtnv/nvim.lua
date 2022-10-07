b53b5a00cb00f4e224ef4a80f554a8e8d982a34a
dngtnv <kakalupin@gmail.com>
2022-10-07 11:31:00 +0700

    refactor: show hidden file (nvim-tree, telescope)

--- lua/telescope-config/init.lua
+++ lua/telescope-config/init.lua
@@ -10,4 +10,15 @@ telescope.setup({
 		border = {},
 		borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
 	},
+	pickers = {
+		find_files = {
+			-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+			find_command = { "rg", "--files", "--no-ignore-vcs", "--hidden", "--glob", "!.git/*" },
+		},
+	},
+	file_ignore_patterns = {
+		"node_modules",
+		"build",
+		"dist",
+	},
 })
