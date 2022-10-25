bf8d66a97058c4438559bfffc841f61a7de63129
dngtnv <kakalupin@gmail.com>
2022-10-25 22:58:42 +0700

    refactor(cmp_nvim_lsp): update config

--- lua/lsp-config/language-servers.lua
+++ lua/lsp-config/language-servers.lua
@@ -1,34 +1,34 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-    ensure_installed = {
-        "html",
-        "cssls",
-        "tsserver",
-        "tailwindcss",
-        "emmet_ls",
-        "sumneko_lua",
-    },
+	ensure_installed = {
+		"html",
+		"cssls",
+		"tsserver",
+		"tailwindcss",
+		"emmet_ls",
+		"sumneko_lua",
+	},
 })
 local nvim_lsp = require("lspconfig")
 
 -- Custom diagnostics sign & message
 vim.diagnostic.config({
-    virtual_text = {
-        prefix = "●", -- Could be '■', '▎', 'x'
-        source = "always", -- Or "if_many"
-    },
-    float = {
-        source = "always", -- Or "if_many"
-    },
-    signs = true,
-    underline = true,
-    update_in_insert = true, -- Update diagnostics in insert mode
-    severity_sort = false,
+	virtual_text = {
+		prefix = "●", -- Could be '■', '▎', 'x'
+		source = "always", -- Or "if_many"
+	},
+	float = {
+		source = "always", -- Or "if_many"
+	},
+	signs = true,
+	underline = true,
+	update_in_insert = true, -- Update diagnostics in insert mode
+	severity_sort = false,
 })
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
-    local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
@@ -40,8 +40,8 @@ vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts) ]]
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(_, bufnr) -- (client, bufnr)
-    -- Show diagnostic on hover
-    --[[ vim.api.nvim_create_autocmd("CursorHold", {
+	-- Show diagnostic on hover
+	--[[ vim.api.nvim_create_autocmd("CursorHold", {
         buffer = bufnr,
         callback = function()
             local opts = {
@@ -55,13 +55,13 @@ local on_attach = function(_, bufnr) -- (client, bufnr)
             vim.diagnostic.open_float(nil, opts)
         end,
     }) ]]
-    -- Enable completion triggered by <c-x><c-o>
-    vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+	-- Enable completion triggered by <c-x><c-o>
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-    -- Mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local bufopts = { noremap = true, silent = true, buffer = bufnr }
-    --[[ vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+	-- Mappings.
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	--[[ vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
 	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
 	vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
 	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
@@ -75,41 +75,41 @@ local on_attach = function(_, bufnr) -- (client, bufnr)
 	vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
 	vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
 	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts) ]]
-    vim.keymap.set("n", "<space>fm", vim.lsp.buf.format, bufopts)
+	vim.keymap.set("n", "<space>fm", vim.lsp.buf.format, bufopts)
 end
 
 local lsp_flags = {
-    -- This is the default in Nvim 0.7+
-    debounce_text_changes = 150,
+	-- This is the default in Nvim 0.7+
+	debounce_text_changes = 150,
 }
 
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 nvim_lsp.tsserver.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    flags = lsp_flags,
-    root_dir = function()
-        return vim.loop.cwd()
-    end,
+	on_attach = on_attach,
+	capabilities = capabilities,
+	flags = lsp_flags,
+	root_dir = function()
+		return vim.loop.cwd()
+	end,
 })
 
 local servers = {
-    "html",
-    "cssls",
-    -- "tsserver",
-    "eslint",
-    "tailwindcss",
-    "emmet_ls",
-    "sumneko_lua",
+	"html",
+	"cssls",
+	-- "tsserver",
+	"eslint",
+	"tailwindcss",
+	"emmet_ls",
+	"sumneko_lua",
 }
 
 for _, lsp in ipairs(servers) do
-    nvim_lsp[lsp].setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-        flags = lsp_flags,
-    })
+	nvim_lsp[lsp].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+		flags = lsp_flags,
+	})
 end
