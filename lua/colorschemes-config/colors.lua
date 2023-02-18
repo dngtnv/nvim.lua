85dfd3fef6599cc40a931b79d3274a4b1874986f
dngtnv <kakalupin@gmail.com>
2023-02-18 16:01:56 +0700

    refactor: remove comment & refactor code

--- lua/colorschemes-config/colors.lua
+++ lua/colorschemes-config/colors.lua
@@ -9,25 +9,25 @@ end
 ColorMyBoard() ]]
 local status_ok, tokyo = pcall(require, "tokyonight")
 if not status_ok then
-	print("Tokyonight colorscheme is not installed")
-	return
+  print("Tokyonight colorscheme is not installed")
+  return
 end
 local toggle = true
 
 function ToggleAcrylic()
-	if toggle then
-		tokyo.setup({
-			transparent = true,
-		})
-		vim.cmd.colorscheme("tokyonight")
-		toggle = false
-	else
-		tokyo.setup({
-			transparent = false,
-		})
-		vim.cmd.colorscheme("tokyonight")
-		toggle = true
-	end
+  if toggle then
+    tokyo.setup({
+      transparent = true,
+    })
+    vim.cmd.colorscheme("tokyonight")
+    toggle = false
+  else
+    tokyo.setup({
+      transparent = false,
+    })
+    vim.cmd.colorscheme("tokyonight")
+    toggle = true
+  end
 end
 
 ToggleAcrylic()
