bcc08234d1a0287b3ae256a4bdb14315d81be19c
dngtnv <kakalupin@gmail.com>
2023-02-24 12:37:55 +0700

    refactor: fix indent

--- lua/bufferline-config/init.lua
+++ lua/bufferline-config/init.lua
@@ -1,52 +1,52 @@
-local status_ok, bufferline = pcall(require, 'bufferline')
+local status_ok, bufferline = pcall(require, "bufferline")
 if not status_ok then
-    print('Bufferline is not installed')
-    return
+	print("Bufferline is not installed")
+	return
 end
 
-bufferline.setup{
-    options = {
-        mode = 'buffers', -- set to 'tabs' to only show tabpages instead
-        numbers = 'none', -- ordinal, buffer_id, both
-        number_style = '',
-        close_command = 'Bdelete! %d',
-        right_mouse_command = 'Bdelete! %d',
-        left_mouse_command = 'buffer %d',
-        middle_mouse_command = nil,
-        indicator = {
-            icon = '',
-            style = 'icon', --icon, underline, none 
-        },
-        buffer_close_icon = '',
-        modified_icon = '●',
-        close_icon = '',
-        left_trunc_marker = '',
-        right_trunc_marker = '',
-        --- name_formatter can be used to change the buffer's label in the bufferline.
-        --- Please note some names can/will break the
-        --- bufferline so use this at your discretion knowing that it has
-        --- some limitations that will *NOT* be fixed.
-        name_formatter = function(buf)  -- buf contains a "name", "path" and "bufnr"
-            -- remove extension from markdown files for example
-            if buf.name:match('%.md') then
-                return vim.fn.fnamemodify(buf.name, ':t:r')
-            end
-        end,
-        max_name_length = 18,
-        max_prefix_length = 15,
-        tab_size = 18,
-        diagnostics = false,
-        diagnostics_update_in_insert = false,
-        offsets = {{filetype = 'NvimTree', text = '', text_align = 'left', separator = true}},
-        color_icons = true,
-        show_buffer_icon = true,
-        show_buffer_close_icons = true,
-        show_buffer_default_icon = true,
-        show_close_icon = false,
-        show_tab_indicators = true,
-        persist_buffer_sort = true,
-        separator_style = 'thin', -- slant, thick, thin {'any', 'any'}
-        enforce_regular_tabs = true,
-        always_show_bufferline = true,
-    }
-}
+bufferline.setup({
+	options = {
+		mode = "buffers", -- set to 'tabs' to only show tabpages instead
+		numbers = "none", -- ordinal, buffer_id, both
+		number_style = "",
+		close_command = "Bdelete! %d",
+		right_mouse_command = "Bdelete! %d",
+		left_mouse_command = "buffer %d",
+		middle_mouse_command = nil,
+		indicator = {
+			icon = "",
+			style = "icon", --icon, underline, none
+		},
+		buffer_close_icon = "",
+		modified_icon = "●",
+		close_icon = "",
+		left_trunc_marker = "",
+		right_trunc_marker = "",
+		--- name_formatter can be used to change the buffer's label in the bufferline.
+		--- Please note some names can/will break the
+		--- bufferline so use this at your discretion knowing that it has
+		--- some limitations that will *NOT* be fixed.
+		name_formatter = function(buf) -- buf contains a "name", "path" and "bufnr"
+			-- remove extension from markdown files for example
+			if buf.name:match("%.md") then
+				return vim.fn.fnamemodify(buf.name, ":t:r")
+			end
+		end,
+		max_name_length = 18,
+		max_prefix_length = 15,
+		tab_size = 18,
+		diagnostics = false,
+		diagnostics_update_in_insert = false,
+		offsets = { { filetype = "NvimTree", text = "", text_align = "left", separator = true } },
+		color_icons = true,
+		show_buffer_icon = true,
+		show_buffer_close_icons = true,
+		show_buffer_default_icon = true,
+		show_close_icon = false,
+		show_tab_indicators = true,
+		persist_buffer_sort = true,
+		separator_style = "thin", -- slant, padded_slant, thick, thin, or custom characters {'|', '|'}
+		enforce_regular_tabs = true,
+		always_show_bufferline = true,
+	},
+})
--- lua/lsp-config/nvim-cmp.lua
+++ lua/lsp-config/nvim-cmp.lua
@@ -6,28 +6,29 @@ local t = luasnip.text_node
 local i = luasnip.insert_node
 
 luasnip.add_snippets("typescriptreact", {
-	s("log", {
-		t("console.log("),
-		i(1, ""),
-		t(","),
-		i(2, ""),
-		t(")"),
-	}),
+  s("log", {
+    t("console.log("),
+    i(1, ""),
+    t(","),
+    i(2, ""),
+    t(")"),
+  }),
 })
 local cmp = require("cmp")
 local lspkind = require("lspkind")
+require("luasnip.loaders.from_vscode").lazy_load()
 
 local function border(hl_name)
-	return {
-		{ "┌", hl_name },
-		{ "─", hl_name },
-		{ "┐", hl_name },
-		{ "│", hl_name },
-		{ "┘", hl_name },
-		{ "─", hl_name },
-		{ "└", hl_name },
-		{ "│", hl_name },
-		--[[ { "╭", hl_name },
+  return {
+    { "┌", hl_name },
+    { "─", hl_name },
+    { "┐", hl_name },
+    { "│", hl_name },
+    { "┘", hl_name },
+    { "─", hl_name },
+    { "└", hl_name },
+    { "│", hl_name },
+    --[[ { "╭", hl_name },
 		{ "─", hl_name },
 		{ "╮", hl_name },
 		{ "│", hl_name },
@@ -35,78 +36,106 @@ local function border(hl_name)
 		{ "─", hl_name },
 		{ "╰", hl_name },
 		{ "│", hl_name }, ]]
-	}
+  }
 end
 
 -- nvim-cmp setup
 cmp.setup({
-	snippet = {
-		expand = function(args)
-			luasnip.lsp_expand(args.body)
-		end,
-	},
-	completion = {
-		completeopt = "menu,menuone,noinsert",
-	},
-	mapping = cmp.mapping.preset.insert({
-		["<C-d>"] = cmp.mapping.scroll_docs(-4),
-		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
-		["<C-e>"] = cmp.mapping.abort(),
-		["<CR>"] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
-		}),
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
-				luasnip.expand_or_jump()
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-	}),
-	sources = {
-		{ name = "nvim_lsp" },
-		{ name = "luasnip" },
-		{ name = "buffer" },
-	},
-	window = {
-		completion = {
-			border = border("CmpBorder"), -- Add border
-			winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
-			col_offset = 0,
-			side_padding = 0,
-		},
-		documentation = {
-			border = border("CmpBorder"), -- Add border for documentation
-		},
-	},
-	formatting = {
-		fields = { "kind", "abbr", "menu" },
-		format = function(entry, vim_item)
-			local kind =
-				lspkind.cmp_format({ mode = "symbol_text", maxwidth = 50, ellipsis_char = "..." })(entry, vim_item)
-			local strings = vim.split(kind.kind, "%s", { trimempty = true })
-			kind.kind = " " .. strings[1] .. " "
-			kind.menu = "    (" .. strings[2] .. ")"
+  enabled = function()
+    -- turn off cmp in Telescope buffer
+    local buftype = vim.api.nvim_buf_get_option(0, "buftype")
+    if buftype == "prompt" then
+      return false
+    end
+    -- disable completion in comments
+    local context = require("cmp.config.context")
+    -- keep command mode completion enabled when cursor is in a comment
+    if vim.api.nvim_get_mode().mode == "c" then
+      return true
+    else
+      return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
+    end
+  end,
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  completion = {
+    completeopt = "menu,menuone,noinsert",
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-d>"] = cmp.mapping.scroll_docs( -4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable( -1) then
+        luasnip.jump( -1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  }),
+  sources = {
+    { name = "nvim_lsp", priority = 30, group_index = 2 },
+    { name = "luasnip",  priority = 20, group_index = 2 },
+    { name = "buffer",   priority = 10, group_index = 2 },
+  },
+  window = {
+    completion = {
+      border = border("CmpBorder"), -- Add border
+      winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+      col_offset = 0,
+      side_padding = 0,
+    },
+    documentation = {
+      border = border("CmpBorder"), -- Add border for documentation
+    },
+  },
+  experimental = {
+    ghost_text = false,
+  },
+  formatting = {
+    fields = { "kind", "abbr", "menu" },
+    format = function(entry, vim_item)
+      local kind = lspkind.cmp_format({
+            mode = "symbol_text",
+            maxwidth = 50,
+            ellipsis_char = "...",
+          })(entry, vim_item)
+      local strings = vim.split(kind.kind, "%s", { trimempty = true })
+      kind.kind = " " .. strings[1] .. " "
+      kind.menu = "    (" .. strings[2] .. ")"
 
-			return kind
-		end,
-	},
-	--[[ formatting = {
+      return kind
+    end,
+  },
+  --[[ formatting = {
 		format = lspkind.cmp_format({
 			mode = "symbol_text", -- show only symbol annotations [symbol | text | symbol_text | text_symbol]
+			menu = {
+				buffer = "[Buffer]",
+				nvim_lsp = "[LSP]",
+				luasnip = "[LuaSnip]",
+				nvim_lua = "[Lua]",
+				latex_symbols = "[Latex]",
+			},
 			maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
 			ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
 			-- The function below will be called before any actual modifications from lspkind
