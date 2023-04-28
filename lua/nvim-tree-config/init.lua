c1387b61c490b3ad645cb815b6344edb01e81dfc
dngtnv <kakalupin@gmail.com>
2023-04-28 15:23:49 +0700

    centering float nvimtree

--- lua/nvim-tree-config/init.lua
+++ lua/nvim-tree-config/init.lua
@@ -4,6 +4,9 @@ if not status_ok then
 	return
 end
 
+local HEIGHT_RATIO = 0.8 -- You can change this
+local WIDTH_RATIO = 0.5 -- You can change this too
+
 nvimtree.setup({
 	auto_reload_on_write = true,
 	create_in_closed_folder = false,
@@ -20,15 +23,28 @@ nvimtree.setup({
 		float = {
 			enable = true,
 			quit_on_focus_loss = true,
-			open_win_config = {
-				relative = "editor",
-				border = "rounded",
-				width = 30,
-				height = 30,
-				row = 1,
-				col = 1,
-			},
+			open_win_config = function()
+				local screen_w = vim.opt.columns:get()
+				local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+				local window_w = screen_w * WIDTH_RATIO
+				local window_h = screen_h * HEIGHT_RATIO
+				local window_w_int = math.floor(window_w)
+				local window_h_int = math.floor(window_h)
+				local center_x = (screen_w - window_w) / 2
+				local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+				return {
+					border = "rounded",
+					relative = "editor",
+					row = center_y,
+					col = center_x,
+					width = window_w_int,
+					height = window_h_int,
+				}
+			end,
 		},
+		width = function()
+			return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+		end,
 	},
 	renderer = {
 		add_trailing = false,
