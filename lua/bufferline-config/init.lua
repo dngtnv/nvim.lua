ad1534b34625fa894005e8337c49ba0bb468955a
dngtnv <kakalupin@gmail.com>
2023-05-08 10:51:25 +0700

    update config bufferline

--- lua/bufferline-config/init.lua
+++ lua/bufferline-config/init.lua
@@ -1,44 +1,44 @@
 local status_ok, bufferline = pcall(require, "bufferline")
 if not status_ok then
-	print("Bufferline is not installed")
-	return
+  print("Bufferline is not installed")
+  return
 end
 
 bufferline.setup({
-	options = {
-		mode = "buffers", -- set to 'tabs' to only show tabpages instead
-		numbers = "none", -- ordinal, buffer_id, both
-		number_style = "",
-		close_command = "Bdelete! %d",
-		right_mouse_command = "vertical sbuffer %d",
-		left_mouse_command = "buffer %d",
-		middle_mouse_command = "Bdelete! %d",
-		indicator = {
-			icon = "▎",
-			style = "icon", --icon, underline, none
-		},
-		buffer_close_icon = "",
-		modified_icon = "●",
-		close_icon = "",
-		left_trunc_marker = "",
-		right_trunc_marker = "",
-		--- name_formatter can be used to change the buffer's label in the bufferline.
-		--- Please note some names can/will break the
-		--- bufferline so use this at your discretion knowing that it has
-		--- some limitations that will *NOT* be fixed.
-		name_formatter = function(buf) -- buf contains a "name", "path" and "bufnr"
-			-- remove extension from markdown files for example
-			if buf.name:match("%.md") then
-				return vim.fn.fnamemodify(buf.name, ":t:r")
-			end
-		end,
-		max_name_length = 18,
-		max_prefix_length = 15,
-		tab_size = 18,
-		diagnostics = false,
-		diagnostics_update_in_insert = false,
-		offsets = { { filetype = "NvimTree", text = "", text_align = "left", separator = true } },
-		--[[ color_icons = true,
+  options = {
+    mode = "buffers", -- set to 'tabs' to only show tabpages instead
+    numbers = "none", -- ordinal, buffer_id, both
+    number_style = "",
+    close_command = "Bdelete! %d",
+    right_mouse_command = "vertical sbuffer %d",
+    left_mouse_command = "buffer %d",
+    middle_mouse_command = "Bdelete! %d",
+    indicator = {
+      icon = "▎",
+      style = "none", --icon, underline, none
+    },
+    buffer_close_icon = "",
+    modified_icon = "●",
+    close_icon = "",
+    left_trunc_marker = "",
+    right_trunc_marker = "",
+    --- name_formatter can be used to change the buffer's label in the bufferline.
+    --- Please note some names can/will break the
+    --- bufferline so use this at your discretion knowing that it has
+    --- some limitations that will *NOT* be fixed.
+    name_formatter = function(buf) -- buf contains a "name", "path" and "bufnr"
+      -- remove extension from markdown files for example
+      if buf.name:match("%.md") then
+        return vim.fn.fnamemodify(buf.name, ":t:r")
+      end
+    end,
+    max_name_length = 18,
+    max_prefix_length = 15,
+    tab_size = 18,
+    diagnostics = false, -- "nvim_lsp" | "coc"
+    diagnostics_update_in_insert = false,
+    offsets = { { filetype = "NvimTree", text = "", text_align = "left", separator = true } },
+    --[[ color_icons = true,
 		--get_element_icon = function(element)
 			-- element consists of {filetype: string, path: string, extension: string, directory: string}
 			-- This can be used to change how bufferline fetches the icon
@@ -50,14 +50,19 @@ bufferline.setup({
 			[[ local custom_map = {my_thing_ft: {icon = "my_thing_icon", hl}}
       return custom_map[element.filetype]
 		end, ]]
-		show_buffer_icon = true,
-		show_buffer_close_icons = true,
-		show_buffer_default_icon = true,
-		show_close_icon = true,
-		show_tab_indicators = true,
-		persist_buffer_sort = true,
-		separator_style = "thin", -- "slant" | "slope" | "thick" | "thin" | or custom characters {'|', '|'}
-		enforce_regular_tabs = true,
-		always_show_bufferline = true,
-	},
+    show_buffer_icon = true,
+    show_buffer_close_icons = true,
+    show_buffer_default_icon = true,
+    show_close_icon = true,
+    show_tab_indicators = true,
+    persist_buffer_sort = true,
+    separator_style = "thin", -- "slant" | "padded_slant" | "slope" | "thick" | "thin" | or custom characters {'|', '|'}
+    enforce_regular_tabs = true,
+    always_show_bufferline = true,
+    hover = {
+      enabled = true,
+      delay = 200,
+      reveal = { "close" },
+    },
+  },
 })
