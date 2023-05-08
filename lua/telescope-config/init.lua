da5b9078acf89edfdcc3a111573e5fc9bcac64bc
dngtnv <kakalupin@gmail.com>
2023-05-08 10:42:21 +0700

    change slection caret & prompt prefix icons

--- lua/telescope-config/init.lua
+++ lua/telescope-config/init.lua
@@ -1,33 +1,35 @@
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
-    print("Telescope is not installed")
-    return
+  print("Telescope is not installed")
+  return
 end
 
 telescope.setup({
-    defaults = {
-        vimgrep_arguments = {
-            "rg",
-            "--color=never",
-            "--no-heading",
-            "--with-filename",
-            "--line-number",
-            "--column",
-            "--smart-case",
-        },
-        winblend = 0,
-        border = {},
-        borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
+  defaults = {
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
     },
-    pickers = {
-        find_files = {
-            -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
-            find_command = { "rg", "--files", "--hidden", "--glob", "!.git/*" },
-        },
+    winblend = 0,
+    prompt_prefix = "❯ ",
+    selection_caret = " ",
+    border = {},
+    borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
+  },
+  pickers = {
+    find_files = {
+      -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+      find_command = { "rg", "--files", "--hidden", "--glob", "!.git/*" },
     },
-    file_ignore_patterns = {
-        "node_modules",
-        "build",
-        "dist",
-    },
+  },
+  file_ignore_patterns = {
+    "node_modules",
+    "build",
+    "dist",
+  },
 })
