7611855428262592aedbba6bc483e43e70c33b25
dngtnv <kakalupin@gmail.com>
2023-07-14 11:37:43 +0700

    colorscheme config

--- lua/colorschemes/tokyonight.lua
+++ lua/colorschemes/tokyonight.lua
@@ -1,1 +1,55 @@
-vim.cmd([[colorscheme tokyonight]])
+local tokyonight = require("tokyonight")
+local toggle = true
+
+function ToggleTrans()
+  if toggle then
+    tokyonight.setup({
+      style = "storm",
+      transparent = toggle,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+      on_highlights = function(hl, _)
+        hl.NvimTreeSpecialFile = {
+          fg = "#ff9e64",
+          underline = false,
+        }
+      end,
+    })
+    vim.cmd.colorscheme("tokyonight")
+    toggle = not toggle
+  else
+    tokyonight.setup({
+      style = "storm",
+      transparent = false,
+      styles = {
+        sidebars = "dark",
+        floats = "dark",
+      },
+      on_highlights = function(hl, _)
+        hl.NvimTreeSpecialFile = {
+          fg = "#ff9e64",
+          underline = false,
+        }
+      end,
+    })
+    vim.cmd.colorscheme("tokyonight")
+    toggle = not toggle
+  end
+end
+
+ToggleTrans()
+
+-- tokyonight.setup({
+--   style = "storm",
+--   transparent = false,
+--   on_highlights = function(hl, _)
+--     hl.NvimTreeSpecialFile = {
+--       fg = "#ff9e64",
+--       underline = false,
+--     }
+--   end,
+-- })
+
+-- vim.cmd([[colorscheme tokyonight]])
